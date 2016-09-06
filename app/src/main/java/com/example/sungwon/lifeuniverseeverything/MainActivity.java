694a53406e6f1eb67ad3164ef79cff2e320bd463
Sungzors <s1chun3636@gmail.com>
2016-09-06 12:02:48 -0700

    created main activity with animation in the background

--- app/src/main/java/com/example/sungwon/lifeuniverseeverything/MainActivity.java
+++ app/src/main/java/com/example/sungwon/lifeuniverseeverything/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.sungwon.lifeuniverseeverything;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView mLuedpic;
+    TextView mStart;
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLuedpic = (ImageView)findViewById(R.id.luedpic);
+        mStart = (TextView)findViewById(R.id.StartButton);
+        animation = AnimationUtils.loadAnimation(this, R.anim.mainmenuanim);
+        mLuedpic.startAnimation(animation);
     }
 }
