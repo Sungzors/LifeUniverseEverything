c3dfd421df89420c94e44a363ee0be72e753dc9b
Sungzors <s1chun3636@gmail.com>
2016-09-08 18:52:35 -0700

    everything works

--- app/src/main/java/com/example/sungwon/lifeuniverseeverything/AddThingActivity.java
+++ app/src/main/java/com/example/sungwon/lifeuniverseeverything/AddThingActivity.java
@@ -1,8 +1,10 @@
 package com.example.sungwon.lifeuniverseeverything;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,15 +14,17 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class AddThingActivity extends AppCompatActivity {
+public class AddThingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     ImageView mLUEpic;
     EditText mNameTextAdd;
     Spinner mCatSpinner;
     EditText mTagAdd;
-    EditText mCatAdd;
+    EditText mRevAdd;
     RatingBar mRatingBar;
     Button mAddButton;
     SQLHelper mHelper;
+    String catselect;
+    int mRating = 0;
 
 
     @Override
@@ -30,20 +34,47 @@ public class AddThingActivity extends AppCompatActivity {
         mNameTextAdd = (EditText)findViewById(R.id.addName);
         mCatSpinner = (Spinner)findViewById(R.id.catSpinner);
         mTagAdd = (EditText)findViewById(R.id.tagAdd);
-        mCatAdd = (EditText)findViewById(R.id.catAdd);
+        mRevAdd = (EditText)findViewById(R.id.revAdd);
         mRatingBar = (RatingBar)findViewById(R.id.ratingBar);
         mAddButton = (Button)findViewById(R.id.addButton);
         mHelper = new SQLHelper(this);
         ArrayList<String> catarray= SQLHelper.getInstance(this).getAllCats();
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter.addAll(catarray);
         mCatSpinner.setAdapter(adapter);
+        mCatSpinner.setOnItemSelectedListener(this);
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRating = (int) (v * 2);
+            }
+        });
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String name = mNameTextAdd.getText().toString();
+                String tags = mTagAdd.getText().toString();
+                String rev = mRevAdd.getText().toString();
+                int cid = mHelper.getCategoryid(catselect);
 
+                Everything thing = new Everything(cid, name, mRating, rev, tags );
+                int i = mHelper.insertEverything(thing);
+                mHelper.insertTag(thing, i);
+
+                Intent intent = new Intent(AddThingActivity.this, EverythingListActivity.class);
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        catselect = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        catselect = "state of being";
     }
 }
