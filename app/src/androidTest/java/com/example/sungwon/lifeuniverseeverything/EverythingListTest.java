c7ad7931fd41711d13e2a347d293849732ff2ce0
Sungzors <s1chun3636@gmail.com>
2016-09-09 14:03:45 -0700

    readme

--- app/src/androidTest/java/com/example/sungwon/lifeuniverseeverything/EverythingListTest.java
+++ app/src/androidTest/java/com/example/sungwon/lifeuniverseeverything/EverythingListTest.java
@@ -1,6 +1,7 @@
 package com.example.sungwon.lifeuniverseeverything;
 
 import android.app.Application;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
@@ -9,10 +10,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.startsWith;
 
 /**
  * Created by SungWon on 9/9/2016.
@@ -27,8 +34,29 @@ public class EverythingListTest extends ApplicationTestCase<Application> {
     public ActivityTestRule<EverythingListActivity> mActivityRule = new ActivityTestRule<EverythingListActivity>(EverythingListActivity.class);
 
     @Test
-    public void testclickable() throws Exception {
-        onView(withId(R.id.StartButton))
-                .check(matches(isClickable()));
+    public void testAdd() throws Exception {
+        onView(withId(R.id.fab))
+                .perform(click());
+
+        onView(withId(R.id.addName))
+                .perform(clearText(), typeText("Testing"), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.tagAdd))
+                .perform(clearText(), typeText("just, testing, stuff"), ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.revAdd))
+                .perform(clearText(), typeText("Type stuff here, too, man"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.ratingBar))
+                .perform(click());
+
+        onView(withId(R.id.addButton))
+                .perform(click());
+
+        onData(anything())
+                .inAdapterView(withId(R.id.list_view))
+                .atPosition(SQLHelper.getInstance(getContext()).getLastIDET())
+                .onChildView(withId(R.id.everythingName))
+                .check(matches(withText(startsWith("Testing"))));
     }
+
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
-readme.md
+readme for Life, Universe, Everything app:
+
+User stories:
+I want to be able to rate and review my dog whenever
+I want to see what I've reviewed so far after I've reviewed it
+I want to sort by the things with the best ratings
+I want to be able to search based on categories
+I want to just simply rate a thing and nothing more
+I want to be able to choose from a small group of pre-determined categories
+I want categories to be vague and in general
+I want to set tags
+
+This is an app about everything.  Review whatever you want, however you want, whenever you want.  You will be able to add tags, write a review, rate it, and eventually attach a picture to it.
+
+As the app starts, please play start to initiate the app.
+
+Immediately as the app starts, there will be a display of all of the reviews made so far. They can be sorted by clicking on the 3 buttons on top.  By clicking the search icon, you can also search for a specific "Everything."  You can set up the search criteria by clicking the button right below the search button.
+
+Meanwhile, if you wish to see more details about a thing, touch the item on the list, and a more detailed dialog box will appear.  Please press the back button if you wish to return to the simplified list.
+
